f42dd692669136456e8cf750d860a22e4c334828
Kevin Hoàng <hello@kev1n.co>
2023-10-02 20:59:26 +0200

    Create dearimgui.h

---
+++ src/dearimgui.h
@@ -0,0 +1,37 @@
+#ifndef DEARIMGUI_H
+#define DEARIMGUI_H
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+extern GLFWwindow* window;
+
+void imgui_init() {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 410");
+}
+
+void imgui_update(std::function<void()> render) {
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+  render();
+  ImGui::Render();
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void imgui_destroy() {
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+}
+
+#endif  // DEARIMGUI_H
