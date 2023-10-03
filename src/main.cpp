553d989f743d10d92152f6751baaab1af3cae57b
Kevin Hoàng <hello@kev1n.co>
2023-10-03 12:19:49 +0200

    OpenGL draw

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,62 @@
 
 #include "dearimgui.h"
 #include "window.h"
+#include "shader.h"
+
+static char* programName = "Fraktale";
+static int width = 1280;
+static int height = 720;
 
 GLFWwindow* window = nullptr;
 
-int main(int argc, char** argv) {
-  std::cout << "Hello, World!" << std::endl;
+static GLuint VAO, VBO, EBO;
+static GLfloat vertices[] = {
+    -1.0f, -1.0f, 0.0f,  // A
+    1.0f,  -1.0f, 0.0f,  // B
+    1.0f,  1.0f,  0.0f,  // C
+    -1.0f, 1.0f,  0.0f,  // D
+};
+static GLint indices[] = {0, 1, 2, 2, 3, 0};
 
-  if (window_init(800, 600, "Hello, World!") != 0) {
+static Shader* shader = new Shader();
+
+int main(int argc, char** argv) {
+  if (window_init(width, height, programName) != 0) {
     return 1;
   }
   imgui_init();
 
+  // Vertex Array
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  // Vertex Buffer
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+
+  // Element Buffer
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  // Shader
+  shader->Init();
+  shader->Load(GL_VERTEX_SHADER, "VertexShader.vert");
+  shader->Load(GL_FRAGMENT_SHADER, "FragmentShader.frag");
+  shader->Link();
+
   while (!window_should_close()) {
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
+    glDisableVertexAttribArray(0);
+
+    shader->Attach();
 
     imgui_update([]() {
       ImGui::Begin("Hello, World!");
